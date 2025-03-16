497f4788a63ce0bab23bd63974a0c786806f76f7
notadoctortired <alyxpeacockemail@gmail.com>
2025-03-16 00:44:29 +0000

    Added materials to walls+floor & began work on obstacles

---
+++ scripts/Kill Scripts/kill_plain.gd
@@ -0,0 +1,11 @@
+extends Area3D
+
+var plr = ""
+
+func _ready():
+	plr = get_parent().get_parent().get_node("Player")
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body):
+	if body == plr:
+		get_tree().reload_current_scene()
--- scripts/kill_plain.gd
+++
@@ -1,7 +0,0 @@
-extends Area3D
-
-func _ready():
-	body_entered.connect(_on_body_entered)
-
-func _on_body_entered(body):
-	get_tree().reload_current_scene()
