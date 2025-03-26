792022762ccca5c63b05b39c9e1dca4dc4fb3cc4
notadoctortired <alyxpeacockemail@gmail.com>
2025-03-26 09:09:37 +0000

    added randomness to car speed

--- scripts/car.gd
+++ scripts/car.gd
@@ -1,6 +1,6 @@
 extends RigidBody3D
 
-@export var speed = 20
+var speed = 1
 var timer = 1
 var respawn = false
 var origin_pos: Vector3 = Vector3.ZERO
@@ -9,9 +9,9 @@ func _ready():
 	origin_pos = get_parent().get_node("Car_Origin").position
 
 func _integrate_forces(PhysicsDirectBodyState3D):
+	print(speed)
 	lock_rotation = 1
-	linear_velocity.x += 1
-	print(linear_velocity.x)
+	linear_velocity.x += speed
 	
 	if respawn:
 		freeze = 1
@@ -26,5 +26,6 @@ func _process(delta):
 		respawn = true
 	
 	if respawn:
+		speed = randf_range(1,3)
 		position = origin_pos
 		respawn = false
