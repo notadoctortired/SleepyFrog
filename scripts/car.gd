ba6923d64de449906802e96435cd992c3787d3ff
Peacock <M2300497@middlesbro.ac.uk>
2025-03-31 10:03:32 +0100

    commit changes

--- scripts/car.gd
+++ scripts/car.gd
@@ -3,6 +3,7 @@ extends RigidBody3D
 @export var speed = 20
 var timer = 1
 var respawn = false
+var ran_speed = 1
 var origin_pos: Vector3 = Vector3.ZERO
 
 func _ready():
@@ -10,7 +11,7 @@ func _ready():
 
 func _integrate_forces(PhysicsDirectBodyState3D):
 	lock_rotation = 1
-	linear_velocity.x += 1
+	linear_velocity.x += ran_speed
 	print(linear_velocity.x)
 	
 	if respawn:
@@ -28,3 +29,5 @@ func _process(delta):
 	if respawn:
 		position = origin_pos
 		respawn = false
+		ran_speed = RandomNumberGenerator.new(0,2)
+		
