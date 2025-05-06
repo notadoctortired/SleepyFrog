60755b8f1bed0dea85ca5b6cafebc4e7663d8514
notadoctortired <alyxpeacockemail@gmail.com>
2025-05-06 16:21:13 +0100

    final commit

--- scripts/timer.gd
+++ scripts/timer.gd
@@ -1,11 +1,8 @@
 extends Label
 
-@export var timer_value = 120
+@export var timer_value = 0
 
 func _process(delta):
 	text = str(snapped(timer_value,0.1))
 	
-	timer_value -= 1*delta
-	
-	if timer_value <= 0:
-		get_tree().reload_current_scene()
+	timer_value += 1*delta
